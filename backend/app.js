f6caec7217884b5f81a4dfaf91934f7a472d0c51
TahmidShawn7 <md.tahmid022@gmail.com>
2026-05-31 18:46:38 +0600

    fix: fix wrong route at backend

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,17 @@ const __dirname = path.dirname(__filename);
 app.use("/api/bikes", bikesRouter);
 
 // serve frontend
-app.use(express.static(path.join(__dirname, "/frontend")));
+// app.use(express.static(path.join(__dirname, "/frontend")));
 
 // fallback
+// app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, "/frontend/index.html"));
+// });
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend/index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 export default app;
