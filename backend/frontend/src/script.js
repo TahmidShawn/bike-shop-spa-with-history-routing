240c1238979fc236667acc18147ce5986345cff2
TahmidShawn7 <md.tahmid022@gmail.com>
2026-06-02 11:08:30 +0600

    feat: setup tailwind css

--- backend/frontend/src/script.js
+++ backend/frontend/src/script.js
@@ -6,14 +6,17 @@ const root = document.querySelector("#root");
 const nav_container = document.querySelector("#nav_container");
 const footer_container = document.querySelector("#footer_container");
 
+// render nav and footer
 nav_container.innerHTML = navbar();
 footer_container.innerHTML = footer();
 
+// render dynamic contain
 const renderFunc = async () => {
     root.innerHTML = "<p>Loading...</p>";
     root.innerHTML = await router();
 };
 
+// handle spa routing
 document.addEventListener("click", async (e) => {
     const link = e.target.closest("[data-link]");
     if (!link) return;
